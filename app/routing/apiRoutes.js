06f0540029eda0b81baa537c981b51c137dcc8dd
Cwander83 <chriswandermail@gmail.com>
2018-01-27 21:29:26 -0500

    1-27-18(working on sending new survey to array)

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -23,20 +23,16 @@ module.exports = function (app) {
     // API GET Requests
     // Below code handles when users "visit" a page.
     // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
     // ---------------------------------------------------------------------------
     app.get("/api/friends", function (req, res) {
         res.json(friendsData);
+
     });
-
-
 
     // API POST Requests
     // Below code handles when a user submits a form and thus submits data to the server.
     // In each of the below cases, when a user submits form data (a JSON object)
     // ...the JSON is pushed to the appropriate JavaScript array
-    // (ex. User fills out a reservation request... this data is then sent to the server...
-    // Then the server saves the data to the tableData array)
     // ---------------------------------------------------------------------------
 
     app.post("/api/friends", function (req, res) {
@@ -44,9 +40,10 @@ module.exports = function (app) {
         // It will do this by sending out the value "true" have a table
         // req.body is available since we're using the body-parser middleware
         res.json(friendsData);
-        
+        friendsData.push(req.body);
+
         // if (friends.length < 5) {
-           // friends.push(req.body);
+        // friends.push(req.body);
         //     res.json(true);
         // } else {
         //     waitListData.push(req.body);
@@ -54,15 +51,4 @@ module.exports = function (app) {
         // }
     });
 
-    // ---------------------------------------------------------------------------
-    // I added this below code so you could clear out the table while working with the functionality.
-    // Don"t worry about it!
-
-    app.post("/api/clear", function () {
-        // Empty out the arrays of data
-        friends = [];
-        
-
-        console.log(friends);
-    });
 };
