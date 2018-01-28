24d2eb07f5f794d9fc02c9a76f8e3aed3adaa098
Cwander83 <chriswandermail@gmail.com>
2018-01-27 21:30:09 -0500

    1-27-18

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -21,8 +21,6 @@ var friendsData = require("../data/friends.js");
 // ===============================================================================
 module.exports = function (app) {
     // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
     // ---------------------------------------------------------------------------
     app.get("/api/friends", function (req, res) {
         res.json(friendsData);
@@ -36,19 +34,10 @@ module.exports = function (app) {
     // ---------------------------------------------------------------------------
 
     app.post("/api/friends", function (req, res) {
-        // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-        // It will do this by sending out the value "true" have a table
-        // req.body is available since we're using the body-parser middleware
+
         res.json(friendsData);
         friendsData.push(req.body);
 
-        // if (friends.length < 5) {
-        // friends.push(req.body);
-        //     res.json(true);
-        // } else {
-        //     waitListData.push(req.body);
-        //     res.json(false);
-        // }
     });
 
 };
