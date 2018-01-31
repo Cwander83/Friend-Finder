ee3d5a7193ff1ad0c50f0c06b766ba6f10fdfcdf
Cwander83 <chriswandermail@gmail.com>
2018-01-31 18:28:53 -0500

    1-31-18(added some css)

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,21 +3,73 @@
 
 
 var friendsArray = [{
-    // demo object 
-    "name": "Ahmed",
-    "photo": "http://moziru.com/images/fisherman-clipart-18.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-}];
+        // demo object 
+        "name": "Marty",
+        "photo": "http://moziru.com/images/fisherman-clipart-18.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        // demo object 
+        "name": "Larry",
+        "photo": "http://www.picturesof.net/_images_300/Old_Man_Going_Fishing_Royalty_Free_Clipart_Picture_081228-035017-805042.jpg",
+        "scores": [
+            2,
+            1,
+            2,
+            5,
+            2,
+            4,
+            3,
+            2,
+            4,
+            3
+        ]
+    },
+    {
+        // demo object 
+        "name": "Luis",
+        "photo": "http://80skiparty.com/wp-content/uploads/2017/12/fancy-cartoon-guy-fishing-fishing-cartoons-stock-royalty-free-cartoon-guy-fishing.jpg",
+        "scores": [
+            3,
+            4,
+            1,
+            3,
+            2,
+            5,
+            2,
+            1,
+            1,
+            2
+        ]
+    },
+    {
+        // demo object 
+        "name": "Captain Rogers",
+        "photo": "https://images.clipartof.com/small/65645-Royalty-Free-RF-Clipart-Illustration-Of-A-Sexy-Redhead-Woman-Fishing.jpg",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    }
+];
 
 module.exports = friendsArray;
