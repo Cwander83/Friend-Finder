ba7bad2b612638edaea411139fa22cc0b1cc790d
Cwander83 <chriswandermail@gmail.com>
2018-01-30 20:32:05 -0500

    1-30-18(fixed a crazy error)

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -13,7 +13,7 @@ logic.*/
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
-var friendsData = require("../data/friends.js");
+const friendsData = require("../data/friends.js");
 
 
 // ===============================================================================
@@ -35,9 +35,36 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
 
+        // Determine the user's most compatible friend using the following as a guide:
+
+        // Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
+        let user = req.body;
+        console.log(user);
+        
+        let userScore = user["scores[]"];
+        // // Converts the survey scores from str to int
+        for (let i = 0; i < userScore.length; i++) {
+            let scoreInt = parseInt(userScore[i]);
+            userScore[i] = scoreInt;
+        }
+
+        friendsData.push(user);
+        console.log(`user: ${user.name}`);
+        // user.ForEach(element => {
+        //     let scoreInt = 
         res.json(friendsData);
-        friendsData.push(req.body);
+        // });
+
+
+        // With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
+        // Example:
+        // User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        // User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
+        // Total Difference: 2 + 1 + 2 = 5
+        // Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on.
+        
+
+
 
     });
-
 };
