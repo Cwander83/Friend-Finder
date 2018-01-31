970eb1085263c56bff59d4daedf7fffe7d3e5c26
Cwander83 <chriswandermail@gmail.com>
2018-01-30 21:25:53 -0500

    1-30-18(issue fixed again)

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,14 +7,12 @@ JSON of all possible friends.
 survey results. This route will also be used to handle the compatibility 
 logic.*/
 
-
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 const friendsData = require("../data/friends.js");
-
 
 // ===============================================================================
 // ROUTING
@@ -24,7 +22,6 @@ module.exports = function (app) {
     // ---------------------------------------------------------------------------
     app.get("/api/friends", function (req, res) {
         res.json(friendsData);
-
     });
 
     // API POST Requests
@@ -40,12 +37,12 @@ module.exports = function (app) {
         // Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
         let user = req.body;
         console.log(user);
-        
-        let userScore = user["scores[]"];
+
+        //let userScore = user["scores[]"];
         // // Converts the survey scores from str to int
-        for (let i = 0; i < userScore.length; i++) {
-            let scoreInt = parseInt(userScore[i]);
-            userScore[i] = scoreInt;
+        for (let i = 0; i < user.score.length; i++) {
+            let scoreInt = parseInt(user.score[i]);
+            user.score[i] = scoreInt;
         }
 
         friendsData.push(user);
@@ -62,7 +59,26 @@ module.exports = function (app) {
         // User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
         // Total Difference: 2 + 1 + 2 = 5
         // Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on.
-        
+        // let match = 0;
+        // let maxDifference = 40;
+
+        // for (let i = 0; i < friendsData.length - 1; i++) {
+
+        //     let totalDif = 0;
+
+        //     for (let j = 0; j < friendsData[i].scores.length; j++) {
+        //         totalDif += Math.abs(friendsData[i].scores[j] - user.scores[j]);
+        //     }
+
+        //     // The closest match will be the user with the least amount of difference.
+        //     if (totalDif < maxDifference) {
+        //         maxDifference = totalDif;
+        //         match = i;
+        //     } else if (i === 0) {
+        //         match = i;
+        //         maxDifference = totalDif;
+        //     }
+        // }
 
 
 
